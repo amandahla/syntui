fccb05c918520bbdd398a6f9b273a3896283f059
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2024-09-15 17:02:13 -0300

    feat: add menu

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/charmbracelet/bubbles/textinput"
@@ -26,61 +26,138 @@ func main() {
 		defer f.Close() // nolint:errcheck
 	}
 
-	p := tea.NewProgram(initialModel())
+	initialProgram := tea.NewProgram(initialModel())
 
-	m, err := p.Run()
+	m, err := initialProgram.Run()
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	if m, ok := m.(model); ok && m.version != "" {
-		fmt.Printf("\n---\nVersion: %s!\n", m.version)
+	if m, ok := m.(inputModel); ok && m.homeserver != "" {
+		menuProgram := tea.NewProgram(choicesModel{homeserver: m.homeserver})
+		_, err := menuProgram.Run()
+		if err != nil {
+			fmt.Println("Oh no:", err)
+			os.Exit(1)
+		}
 	}
-
-	fmt.Println("Exiting")
 }
 
+// Choices Model
+type choicesModel struct {
+	homeserver string
+	cursor     int
+	choice     string
+	result     string
+	err        string
+}
+
+var choices = []string{"Version", "Help"}
+
+func (m choicesModel) View() string {
+	s := strings.Builder{}
+	s.WriteString("Homeserver: " + m.homeserver + "\n\n")
+	s.WriteString("Actions available: \n\n")
+	for i := 0; i < len(choices); i++ {
+		if m.cursor == i {
+			s.WriteString("(•) ")
+		} else {
+			s.WriteString("( ) ")
+		}
+		s.WriteString(choices[i])
+		s.WriteString("\n")
+	}
+	s.WriteString("\nResult: " + m.result + " \n")
+	if m.err != "" {
+		s.WriteString("\nError: " + m.err + " \n")
+	}
+	s.WriteString("\n(press q to quit)\n")
+
+	return s.String()
+}
+
+func (m choicesModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m choicesModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q", "esc":
+			return m, tea.Quit
+
+		case "enter":
+			// Send the choice on the channel and exit.
+			m.choice = choices[m.cursor]
+			if choices[m.cursor] == "Version" {
+				result, err := checkServer(m.homeserver)
+				if err != nil {
+					m.err = err.Error()
+				}
+				m.result = result
+			}
+			if choices[m.cursor] == "Help" {
+				m.result = "Choose one option"
+				return m, nil
+			}
+			return m, tea.Quit
+
+		case "down", "j":
+			m.cursor++
+			if m.cursor >= len(choices) {
+				m.cursor = 0
+			}
+
+		case "up", "k":
+			m.cursor--
+			if m.cursor < 0 {
+				m.cursor = len(choices) - 1
+			}
+		}
+	}
+
+	return m, nil
+}
+
+// Input Model
 type (
 	errMsg error
 )
 
-type model struct {
-	version   string
-	textInput textinput.Model
-	err       error
+type inputModel struct {
+	homeserver string
+	textInput  textinput.Model
+	err        error
 }
 
-func initialModel() model {
+func initialModel() inputModel {
 	ti := textinput.New()
 	ti.Placeholder = "my-chat.server.com"
 	ti.Focus()
 	ti.CharLimit = 156
 	ti.Width = 20
 
-	return model{
-		version:   "",
-		textInput: ti,
-		err:       nil,
+	return inputModel{
+		homeserver: "",
+		textInput:  ti,
+		err:        nil,
 	}
 }
 
-func (m model) Init() tea.Cmd {
+func (m inputModel) Init() tea.Cmd {
 	return textinput.Blink
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m inputModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.Type {
 		case tea.KeyEnter:
-			version, err := checkServer(m.textInput.Value())
-			if err != nil {
-				return m, tea.Quit
-			}
-			m.version = version
+			m.homeserver = m.textInput.Value()
 			return m, tea.Quit
 		case tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
@@ -96,7 +173,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, cmd
 }
 
-func (m model) View() string {
+func (m inputModel) View() string {
 	return fmt.Sprintf(
 		"Provide Synapse Homeserver URL\n\n%s\n%s",
 		m.textInput.View(),
@@ -122,11 +199,9 @@ func checkServer(inputURL string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	log.Println(string(body))
 	var result ResponseVersion
 	if err := json.Unmarshal(body, &result); err != nil {
 		return "", err
 	}
-	log.Println(result)
 	return result.ServerVersion, nil
 }
